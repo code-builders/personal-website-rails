a00003df242e4f7e6977256b12978cd74193119d
smurfik <smurfik@mlpinit.com>
2015-11-02 15:24:46 -0800

    add projects page

--- personal_website/config/routes.rb
+++ personal_website/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get "/", to: "home#index"
+  get "/projects", to: "home#project"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
